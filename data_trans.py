b30da73a24fac026e70db3f710b3026353f3db7c
Danny Weitekamp <dannyweitekamp@gmail.com>
2020-06-15 00:50:57 -0400

    numpy packing working

--- data_trans.py
+++ data_trans.py
@@ -46,23 +46,39 @@ class Numbalizer(object):
 	number_backmap = Dict.empty(i8,f8)
 	enum_counter = np.array(0)
 
+	@classmethod
+	def __init__(cls):
+		for x in ["<#ANY>",None,'','?sel']:
+			cls.enumerize_value(x)
+
 	@classmethod	
 	def jitstruct_from_spec(cls,name,spec,ind="   "):
 		arg_str = ind*3 + "string_enums, number_enums,\n"
 		arg_str += ind*3 + "string_backmap,number_backmap,\n"
 		arg_str += ind*3 + "enum_counter"
 
-		header = "@njit(cache=True,fastmath=True,nogil=True)\n"
-		header += "def {}_get_enumerized(self,\n{},assert_maps=True):\n".format(name,arg_str)
+		attr_str = ind*3 + ", ".join(spec.keys()) 
 
-		strings = ", ".join(["(self.{},{})".format(k,i) for i,k in enumerate(spec.keys()) if spec[k] == 'string'])
-		numbers = ", ".join(["(self.{},{})".format(k,i) for i,k in enumerate(spec.keys()) if spec[k] == 'number'])
+		header = "@njit(cache=True,fastmath=True,nogil=True)\n"
+		header += "def {}_get_enumerized(\n{},\n{},assert_maps=True):\n".format(name,attr_str,arg_str)
+
+		strings = ", ".join(["({},{})".format(k,i) for i,k in enumerate(spec.keys()) if spec[k] == 'string'])
+		numbers = ", ".join(["({},{})".format(k,i) for i,k in enumerate(spec.keys()) if spec[k] == 'number'])
 
 		body = ind + "enumerized = np.empty(({},),np.int64)\n".format(len(spec.keys()))
 		if(strings != ""):
-			body += ind + "for v,i in [{strings}]:\n".format(strings=strings)
-			body += ind*2 + "if(assert_maps): _assert_map(v, string_enums, string_backmap, enum_counter)\n"
-			body += ind*2 + "enumerized[i] = string_enums[v]\n"
+			body += ind +"if(assert_maps):\n"
+			for i,k in enumerate(spec.keys()):
+				if spec[k] == 'string':
+					body += ind*2 +"_assert_map({}, string_enums, string_backmap, enum_counter)\n".format(k)
+					# body += ind +"enumerized[{}] = string_enums[{}]\n".format(i,k)
+		if(strings != ""):
+			for i,k in enumerate(spec.keys()):
+				if spec[k] == 'string':
+					body += ind +"enumerized[{}] = string_enums[{}]\n".format(i,k)
+			# body += ind + "for v,i in [{strings}]:\n".format(strings=strings)
+			# body += ind*2 + "if(assert_maps): _assert_map(v, string_enums, string_backmap, enum_counter)\n"
+			# body += ind*2 + "enumerized[i] = string_enums[v]\n"
 		if(numbers != ""):	
 			body += ind + "for v,i in [{numbers}]:\n".format(numbers=numbers)
 			body += ind*2 + "if(assert_maps): _assert_map(v, number_enums, number_backmap, enum_counter)\n"
@@ -77,6 +93,8 @@ class Numbalizer(object):
 		l,g = cache_safe_exec(source,gbls={**globals(), **{"_assert_map" : _assert_map}})
 		ge_name =  '{}_get_enumerized'.format(name)
 		get_enumerized = l[ge_name]
+
+		print(get_enumerized.inspect_llvm())
 		nt = l[name]
 
 		#Do this to make nt picklable (see https://stackoverflow.com/questions/16377215/how-to-pickle-a-namedtuple-instance-correctly)
@@ -84,13 +102,15 @@ class Numbalizer(object):
 		nt.__module__ = "__main__"
 
 		def py_get_enumerized(self,assert_maps=True):
-			return get_enumerized(self,cls.string_enums,
-									   cls.number_enums,
-									   cls.string_backmap,
-									   cls.number_backmap,
-									   cls.enum_counter,
-									   assert_maps=assert_maps)
+			return get_enumerized(*self,
+								   string_enums=cls.string_enums,
+								   number_enums=cls.number_enums,
+								   string_backmap=cls.string_backmap,
+								   number_backmap=cls.number_backmap,
+								   enum_counter=cls.enum_counter,
+								   assert_maps=assert_maps)
 		nt.get_enumerized = py_get_enumerized#pytypes.MethodType(_get_enumerized, self) 
+		nt._get_enumerized = get_enumerized#pytypes.MethodType(_get_enumerized, self) 
 
 		return nt
 
@@ -134,6 +154,7 @@ class Numbalizer(object):
 	def state_to_nb_objects(cls,state):
 		out = {}
 		for name, obj_d in state.items():
+			# print(name)
 			out[name] = cls.object_to_nb_object(name,obj_d)
 		return out
 
@@ -154,24 +175,35 @@ class Numbalizer(object):
 			raise ValueError("Could not infer type of %s" % value)
 
 	@classmethod
+	def enumerize(cls,value, typ=None):
+		if(isinstance(value,(list,tuple))):
+			return [cls.enumerize_value(x) for x in value]
+		else:
+			return cls.enumerize_value(x)
+
+	@classmethod
 	def enumerize_value(cls,value, typ=None):
 		if(typ is None): typ = cls.infer_type(value)
 		if(typ == 'string'):
 			value = str(value)
-			if(value not in cls.string_enums):
-				cls.string_enums[value] = cls.enum_counter.item()
-				cls.string_backmap[cls.enum_counter.item()] = value
-				cls.enum_counter += 1
+			_assert_map(value,cls.string_enums,cls.string_backmap,cls.enum_counter)
 			return cls.string_enums[value]
 		elif(typ == 'number'):
 			value = float(value)
-			if(value not in cls.number_enums):
-				cls.number_enums[value] = cls.enum_counter.item()
-				cls.number_backmap[cls.enum_counter.item()] = value
-				cls.enum_counter += 1
+			_assert_map(value,cls.number_enums,cls.number_backmap,cls.enum_counter)
 			return cls.number_enums[value]
 		else:
 			raise ValueError("Unrecognized type %r" % typ)
+
+	@classmethod
+	def unenumerize_value(cls,value, typ=None):
+		if(value in cls.string_backmap):
+			return cls.string_backmap[value]
+		elif(value in cls.number_backmap):
+			return cls.number_backmap[value]
+		else:
+			raise ValueError("No enum for %r." % value)
+
 
 
 
@@ -208,8 +240,8 @@ object_specifications = {
 
 Numbalizer.register_specifications(object_specifications)
 
-
-state = {
+STATE_SIZE = 40
+_state = {
 	"i0" : {
 		"type" : "InterfaceElement",
 		"id" : "i0",
@@ -234,14 +266,16 @@ state = {
 	}
 }
 
+state = {"ie" + str(i) : _state['i0'] for i in range(STATE_SIZE)}
+
 print()
 
-for k,ie in Numbalizer.state_to_nb_objects(state).items():
-	print(ie)
-	print("ENUM",ie.get_enumerized())
+# for k,ie in Numbalizer.state_to_nb_objects(state).items():
+# 	print(ie)
+# 	print("ENUM",ie.get_enumerized())
 
 
-state2 = {
+_state2 = {
 	"a" : {
 		"type" : "Trajectory",
 		"x" : 1,
@@ -259,14 +293,15 @@ state2 = {
 	}
 }
 
+state2 = {"ie" + str(i) : _state2['a'] for i in range(STATE_SIZE)}
+
 
 nb_objects = Numbalizer.state_to_nb_objects(state)
 nb_objects2 = Numbalizer.state_to_nb_objects(state2)
-obj1 = nb_objects['i0']
-obj2 = nb_objects2['a']
+obj1 = nb_objects['ie0']
+obj2 = nb_objects2['ie0']
 	
-def state_to_objs():
-	Numbalizer.state_to_nb_objects(state)
+
 
 def enumerize_obj():
 	obj1.get_enumerized()
@@ -277,12 +312,53 @@ def enumerize_obj_nocheck():
 def enumerize_obj_justnums():
 	obj2.get_enumerized()
 
-def enumerize_objs():
-	Numbalizer.nb_objects_to_enumerized(nb_objects)
+
+def enumerize_value_string():
+	Numbalizer.enumerize_value("159")
+
+def enumerize_value_number():
+	Numbalizer.enumerize_value(159)
+
+def unenumerize_value():
+	Numbalizer.unenumerize_value(2)
 
 
-print("state_to_objs:",time_ms(state_to_objs))
+state_10 = {"ie" + str(i) : _state['i0'] for i in range(10)}
+nb_objects_10 = Numbalizer.state_to_nb_objects(state_10)
+def b10_state_to_objs():
+	Numbalizer.state_to_nb_objects(state_10)
+def b10_enumerize_objs():
+	Numbalizer.nb_objects_to_enumerized(nb_objects_10)
+
+
+state_40 = {"ie" + str(i) : _state['i0'] for i in range(40)}
+nb_objects_40 = Numbalizer.state_to_nb_objects(state_40)
+def b40_state_to_objs():
+	Numbalizer.state_to_nb_objects(state_40)
+def b40_enumerize_objs():
+	Numbalizer.nb_objects_to_enumerized(nb_objects_40)
+
+state_200 = {"ie" + str(i) : _state['i0'] for i in range(200)}
+nb_objects_200 = Numbalizer.state_to_nb_objects(state_200)
+def b200_state_to_objs():
+	Numbalizer.state_to_nb_objects(state_200)
+def b200_enumerize_objs():
+	Numbalizer.nb_objects_to_enumerized(nb_objects_200)
+
+
+print("-----Single Objs------")
 print("enumerize_obj:",time_ms(enumerize_obj))
 print("enumerize_obj_nocheck:",time_ms(enumerize_obj_nocheck))
 print("enumerize_obj_justnums:",time_ms(enumerize_obj_justnums))
-print("enumerize_objs:",time_ms(enumerize_objs))
+print("enumerize_value_string:",time_ms(enumerize_value_string))
+print("enumerize_value_number:",time_ms(enumerize_value_number))
+print("unenumerize_value:",time_ms(enumerize_value_number))
+print("-----State 10 Objs------")
+print("state_to_objs:",time_ms(b10_state_to_objs))
+print("enumerize_objs:",time_ms(b10_enumerize_objs))
+print("-----State 40 Objs------")
+print("state_to_objs:",time_ms(b40_state_to_objs))
+print("enumerize_objs:",time_ms(b40_enumerize_objs))
+print("-----State 200 Objs------")
+print("state_to_objs:",time_ms(b200_state_to_objs))
+print("enumerize_objs:",time_ms(b200_enumerize_objs))

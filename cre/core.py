a2f8d38db1eb9c9b478c338a57833d968599aeff
Danny Weitekamp <dannyweitekamp@gmail.com>
2021-08-29 12:44:27 -0400

    tabs to spaces

--- cre/core.py
+++ cre/core.py
@@ -16,9 +16,9 @@ os.environ['NUMBA_CACHE_DIR'] = os.path.join(os.path.split(cache_dir)[0], "numba
 
 #Monkey Patch Numba so that the builtin functions for List() and Dict() cache between runs 
 def monkey_patch_caching(mod,exclude=[]):
-	for name, val in mod.__dict__.items():
-		if(isinstance(val,Dispatcher) and name not in exclude):
-			val.enable_caching()
+    for name, val in mod.__dict__.items():
+        if(isinstance(val,Dispatcher) and name not in exclude):
+            val.enable_caching()
 
 #They promised to fix this by 0.51.0, so we'll only run it if an earlier release
 # if(tuple([int(x) for x in numba.__version__.split('.')]) < (0,55,0)):
@@ -28,37 +28,37 @@ def monkey_patch_caching(mod,exclude=[]):
 
 #These will be filled in if the user registers a new type
 TYPE_ALIASES = {
-	"float" : 'float64',
-	"flt" : 'float64',
-	"number" : 'float64',
-	"string" : 'unicode_type',
-	"str" : 'unicode_type',
-	'unicode_type' : 'unicode_type',
-	'float64' : 'float64',
+    "float" : 'float64',
+    "flt" : 'float64',
+    "number" : 'float64',
+    "string" : 'unicode_type',
+    "str" : 'unicode_type',
+    'unicode_type' : 'unicode_type',
+    'float64' : 'float64',
 }
 
 DEFAULT_REGISTERED_TYPES = {'float64': float64,
-					 'unicode_type' : unicode_type}
+                     'unicode_type' : unicode_type}
 
-JITSTRUCTS = {}					 
+JITSTRUCTS = {}                  
 
 numba_type_map = {
-	"float64" : float64,
-	"unicode_type" : unicode_type,
-	"string" : unicode_type,
-	"number" : float64,	
+    "float64" : float64,
+    "unicode_type" : unicode_type,
+    "string" : unicode_type,
+    "number" : float64, 
 }
 
 py_type_map = {
-	"float64" : float,
-	"unicode_type" : str,
-	"string" : str,
-	"number" : float,	
+    "float64" : float,
+    "unicode_type" : str,
+    "string" : str,
+    "number" : float,   
 }
 
 numpy_type_map = {
-	"string" : '|U%s',
-	"number" : np.float64,	
+    "string" : '|U%s',
+    "number" : np.float64,  
 }
 
 STRING_DTYPE = np.dtype("U50")
@@ -66,17 +66,32 @@ STRING_DTYPE = np.dtype("U50")
 
 # @intrinsic
 # def _instrinstic_get_null_meminfo(typingctx):
-# 	def codegen(context, builder, sig, args):
-# 		null_meminfo = context.get_constant_null(types.MemInfoPointer(types.voidptr))
-# 		context.nrt.incref(builder, types.MemInfoPointer(types.voidptr), null_meminfo)
-# 		return null_meminfo
-		
-# 	sig = types.MemInfoPointer(types.voidptr)()
+#   def codegen(context, builder, sig, args):
+#       null_meminfo = context.get_constant_null(types.MemInfoPointer(types.voidptr))
+#       context.nrt.incref(builder, types.MemInfoPointer(types.voidptr), null_meminfo)
+#       return null_meminfo
+        
+#   sig = types.MemInfoPointer(types.voidptr)()
 
-# 	return sig, codegen
+#   return sig, codegen
 
 # @njit(cache=True)
 # def _get_null_meminfo():
-# 	return _instrinstic_get_null_meminfo()
+#   return _instrinstic_get_null_meminfo()
 
 # NULL_MEMINFO = _get_null_meminfo()
+
+CRE_TYPE_EXECUTABLE    =int('10000000', 2)
+CRE_TYPE_VAR           =int('10010000', 2)
+CRE_TYPE_OP            =int('10100000', 2)
+CRE_TYPE_CONDITIONS    =int('10110000', 2)
+CRE_TYPE_RULE          =int('11000000', 2)
+
+CRE_TYPE_FACT          = int('00001000', 2)
+CRE_TYPE_ATOM          = int('00001001', 2)
+CRE_TYPE_PRED          = int('00001010', 2)
+
+
+
+
+

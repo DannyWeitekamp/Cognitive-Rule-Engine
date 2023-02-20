4254a01b01e21379b9bdd6068fb38636edab7645
Danny Weitekamp <dannyweitekamp@gmail.com>
2023-02-20 15:16:06 -0700

    clean up test files... added legacy folder

--- tests/conftest.py
+++
@@ -1,5 +0,0 @@
-# This file simply forces pytest_benchmark to use milliseconds as units
-import pytest
-@pytest.hookspec(firstresult=True)
-def pytest_benchmark_scale_unit(config, unit, benchmarks, best, worst, sort):
-    return 'm', 1e3
